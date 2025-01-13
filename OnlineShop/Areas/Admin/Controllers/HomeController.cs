8a3cc9a4dcb060a5b8e3d8a4da06ef11076b970e
Hoangduy2982003 <125873702+Hoangduy2982003@users.noreply.github.com>
2025-01-14 02:05:14 +0700

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers;
+public class HomeController : Controller
+{
+    [Area("Admin")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
